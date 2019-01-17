dfc7699cb8e59687c9aa1e32c11b5115001f6b40
Rupert Adams <RupertNCAdams@aol.com>
2019-01-17 16:05:37 +0000

    wrote second test to assert argument number value

--- fizzBuzz_test.py
+++ fizzBuzz_test.py
@@ -6,3 +6,6 @@ class FizzBuzzTest(unittest.TestCase):
     def test_0_raises_value_error(self):
         with self.assertRaises(ValueError):
             FizzBuzz(0)
+
+    def test_1_does_not_raise_value_error(self):
+        assert FizzBuzz(1).number == 1
